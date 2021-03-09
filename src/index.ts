7ff98e1461178a4e816d5c101b0ad4516b9aa0ca
joaoandrepestre <joaoandre.pestre@poli.ufrj.br>
2021-03-09 09:45:44 +0100

    Adds structure for main route points

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,38 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import {create_user, login, logout} from './user';
 
 const app = express();
-const port = 3000;
+const port: Number = 3000;
 
 // Create user route point
-app.post('/user/create', (req, res) => {
-    res.send('Express working!');
+app.post('/user/create', (req: Request, res: Response) => {
+    const username: String = req.params["username"];
+    const password: String = req.params["password"];
+    if (create_user(username, password))
+        res.send(`User created: ${username}`);
+    else
+        res.send('Failed to create user...');
 });
 
 
 // Login route point
-app.get('/user/login', (req, res) => {
-
+app.get('/user/login', (req: Request, res: Response) => {
+    console.log(req.body);
+    const username: String = req.params["username"];
+    const password: String = req.params["password"];
+    if (login(username, password))
+        res.send(`User logged in: ${username}`);
+    else
+        res.send('Failed to login...');
 });
 
 
 // Logout route point
-app.get('/user/logout', (req, res) => {
-
+app.get('/user/logout', (req: Request, res: Response) => {
+    if (logout())
+        res.send(`User logged out`);
+    else
+        res.send('Failed to logout...');
 });
 
 // Start server listening on given port
