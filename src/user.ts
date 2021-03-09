bb866cb55b3aa5d1d1992db0323cc5c23cf7c8d1
joaoandrepestre <joaoandre.pestre@poli.ufrj.br>
2021-03-09 09:16:14 +0100

    Creates user service

---
+++ src/user.ts
@@ -0,0 +1,12 @@
+
+export const create_user = (username: String, password: String): Boolean => {
+    return true;
+};
+
+export const login = (username: String, password: String): Boolean => {
+    return true;
+};
+
+export const logout = (): Boolean => {
+    return true;
+};
